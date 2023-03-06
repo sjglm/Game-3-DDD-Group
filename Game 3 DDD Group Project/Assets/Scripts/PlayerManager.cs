3f3d2a98edb42ff00037683deefe81f622cc591a
sjglm <mattyglockhart@aol.com>
2023-03-06 01:29:51 +0000

    Multiple changes
    - Added player movement (Using unity's new input system)
    - Added flags
    - Added flag pads
    - Added tags for players (both teams)
    - Added tags for team items (Flags, flag pads)
    - Added prefabs for aforementioned things (players, team items).
    - Added pick up mechanics
    - Added score mechanics

--- Game 3 DDD Group Project/Assets/Scripts/PlayerManager.cs
+++ Game 3 DDD Group Project/Assets/Scripts/PlayerManager.cs
@@ -2,14 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField]
     private float HitPoints = 100f;
+    bool grabbedFlag = false;
+    Collider2D Flag;
+    Vector2 redFlagStartLocation = new Vector2(-7.7f, 0.789f);
+    Vector2 blueFlagStartLocation = new Vector2(8.96f, 1.16f);
 
     private void Update()
     {
-        
+        if(grabbedFlag == true)
+        {
+            CarryFlag();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FlagGrabber(collision);
+        FlagScorer(collision);
+    }
+    private void CarryFlag()
+    {
+        Vector2 flagOffset = new Vector2(0f, 1f);
+        Flag.transform.position = (Vector2)transform.position + flagOffset;
+    }
+    private void FlagGrabber(Collider2D collision)
+    {
+        if (tag == "BluePlayer" && collision.tag == "RedFlag")
+        {
+            grabbedFlag = true;
+            Flag = collision;
+        }
+        if(tag == "RedPlayer" && collision.tag == "Blueflag")
+        {
+            grabbedFlag = true;
+            Flag = collision;
+        }
+    }
+    private void FlagScorer(Collider2D collision)
+    {
+        if(grabbedFlag != true) { }
+        if(grabbedFlag == true)
+        {
+            if(tag == "BluePlayer" && collision.tag == "BlueFlagPad")
+            {
+                grabbedFlag = false;
+                Flag.transform.position = redFlagStartLocation;
+            }
+            else if(tag == "RedPlayer" && collision.tag == "RedFlagPad")
+            {
+                grabbedFlag = false;
+                Flag.transform.position = blueFlagStartLocation;
+            }
+        }
     }
 }
