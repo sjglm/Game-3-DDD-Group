a2c0513b4097b53d54fe3ccd9174a4f34700c98e
sjglm <mattyglockhart@aol.com>
2023-03-23 21:40:26 +0000

    Added score system and win scenes

---
+++ Game 3 DDD Group Project/Assets/Scorer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Scorer : MonoBehaviour
+{
+    public Slider blueScoreSlider;
+    public Slider redScoreSlider;
+
+    private void Start()
+    {
+        blueScoreSlider.value = 0;
+        redScoreSlider.value = 0;
+    }
+
+    private void Update()
+    {
+        ScoreChecker();
+    }
+
+    public void IncreaseRedScore()
+    {
+        redScoreSlider.value += 1;
+    }
+
+    public void IncreaseBlueScore()
+    {
+        blueScoreSlider.value += 1;
+    }
+
+    public void ScoreChecker()
+    {
+        if (blueScoreSlider.value == 1)
+        {
+            SceneManager.LoadScene("BlueTeamWin");
+        }
+        else if(redScoreSlider.value == 1)
+        {
+            SceneManager.LoadScene("RedTeamWin");
+        }
+    }
+}
